cf45c16265b1c784ce615b6e6408fd55704e8b72
Huanqing Xu <huanqingxu@Huanqings-iMac.local>
2020-06-16 16:10:46 -0700

    sarahxu

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,10 @@
 """CPU functionality."""
 
 import sys
-
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -10,11 +13,11 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0]*256
         self.pc = 0
-        self.register = [0]*8
+        self.reg= [0]*8
 
-    def load(self):
+    def load(self,filename):
         """Load a program into memory."""
-
+        '''
         address = 0
 
         # For now, we've just hardcoded a program:
@@ -32,6 +35,21 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+        '''
+        filename = sys.argv[1]
+        with open(filename)as f:
+            for address,line in enumerate(f):
+                line=line.split("#")
+                try:
+                    v = int(line[0],2)
+                except ValueError:
+                    continue
+
+                self.ram[address] = v
+
+
+        
+        
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -39,6 +57,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == MUL:
+            self.reg[reg_a] *=self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -64,9 +84,7 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
+
 
         running = True
         while running:
@@ -74,11 +92,14 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
             if ir == LDI:
-                self.register[operand_a]=operand_b
+                self.reg[operand_a]=operand_b
                 self.pc +=3
             elif ir == PRN:
-                print(self.register[operand_a])
+                print(self.reg[operand_a])
                 self.pc +=2
+            elif ir == MUL:
+                self.alu(ir,operand_a,operand_b)
+                self.pc +=3
             elif ir == HLT:
                 running = False
             else:
